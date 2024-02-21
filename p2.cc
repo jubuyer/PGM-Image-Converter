062a8308ed188740f6d1b5dd0dd11181c5311a83
Jubayer <jubayer7a@gmail.com>
2024-02-21 02:14:03 -0500

    program detects objects that should not exist

--- p2.cc
+++ p2.cc
@@ -16,59 +16,52 @@ using namespace std;
 using namespace ComputerVisionProjects;
 
 void PixelLabeler(Image input, size_t row, size_t col, int& current_pixel_label) {
-  if(input.GetPixel(row, col) != 0) {
-    if(row-1 < 0) { // in first row
-      if(col-1 > 0) { // not in first column
-        //Labeling pixels in first row
-        if(input.GetPixel(row, col) != 0) { // not background pixel
-          if(input.GetPixel(row, col-1) == 0) { //if left pixel is background set new label
-            input.SetPixel(row,col, current_pixel_label); 
-            current_pixel_label-=5; 
-            cout << current_pixel_label << "\n";
-          } else if (input.GetPixel(row, col-1) != 0) { // if left pixel is labeled, relabel current pixel
-            input.SetPixel(row,col, input.GetPixel(row, col-1)); 
-          }
-        }
-      } else { // in first row, first column (first pixel)
-        //Labeling first pixel
-        if(input.GetPixel(row, col) != 0) { // not background pixel
-          input.SetPixel(row,col, current_pixel_label);
-          current_pixel_label-=5; 
-          cout << current_pixel_label << "\n";
-        }
+  if(row-1 < 0) { // in first row
+    if(col-1 > 0) { // not in first column
+      //Labeling pixels in first row
+      // not background pixel
+      if(input.GetPixel(row, col-1) == 0) { //if left pixel is background set new label
+        input.SetPixel(row,col, current_pixel_label); 
+        current_pixel_label-=5; 
+        cout << current_pixel_label << "\n";
+      } else { // if left pixel is labeled, relabel current pixel
+        input.SetPixel(row,col, input.GetPixel(row, col-1)); 
       }
-    } else if (row-1 > 0) { // not in first row
-      if(col-1 > 0) { // not in first column
-        if(input.GetPixel(row, col) != 0) { // not background pixel
-          if(input.GetPixel(row-1, col-1) != 0) { //check diagonally adjacent cell
-            input.SetPixel(row,col, input.GetPixel(row-1, col-1)); 
-          } else if (input.GetPixel(row, col-1) != 0 && input.GetPixel(row-1, col) != 0) { // check double edge case
-            input.SetPixel(row,col, input.GetPixel(row, col-1)); 
-          } else if (input.GetPixel(row, col-1) != 0 && input.GetPixel(row-1, col) == 0) { // check left cell
-            input.SetPixel(row,col, input.GetPixel(row, col-1)); 
-          } else if (input.GetPixel(row, col-1) == 0 && input.GetPixel(row-1, col) != 0) { // check top cell
-            input.SetPixel(row,col, input.GetPixel(row-1, col)); 
-          } else {
-            input.SetPixel(row,col, current_pixel_label);
-            current_pixel_label-=5; 
-            cout << current_pixel_label << "\n";
-          }
-        }
-      } else { // in first column
-      //Labeling pixels in first col (not including first pixel)
-      if(input.GetPixel(row, col) != 0) {
-        if(input.GetPixel(row-1, col) == 0) { // check top pixel
-          input.SetPixel(row,col, current_pixel_label);
-          current_pixel_label-=5; 
-          cout << current_pixel_label << "\n";
-        } else if (input.GetPixel(row, col-1) != 0) {
-          input.SetPixel(row,col, input.GetPixel(row-1, col)); 
-          }
-        }
+    } else { // in first row, first column (first pixel)
+      //Labeling first pixel
+      // not background pixel
+      input.SetPixel(row,col, current_pixel_label);
+      current_pixel_label-=5; 
+      cout << current_pixel_label << "\n";
+    }
+  } else { // not in first row
+    if (col-1 < 0) { // in first column
+    //Labeling pixels in first col (not including first pixel)  
+      if(input.GetPixel(row-1, col) == 0) { // check top pixel
+        input.SetPixel(row,col, current_pixel_label);
+        current_pixel_label-=5; 
+        cout << current_pixel_label << "\n";
+      } else {
+        input.SetPixel(row,col, input.GetPixel(row-1, col)); 
+      }
+    } else { // not in first column and not background pixel
+      if(input.GetPixel(row-1, col-1) != 0) { //check diagonally adjacent cell
+        input.SetPixel(row,col, input.GetPixel(row-1, col-1)); 
+      } else if (input.GetPixel(row, col-1) != 0 && input.GetPixel(row-1, col) != 0) { // check double edge case
+        input.SetPixel(row,col, input.GetPixel(row, col-1)); 
+      } else if (input.GetPixel(row, col-1) != 0 && input.GetPixel(row-1, col) == 0) { // check left cell
+        input.SetPixel(row,col, input.GetPixel(row, col-1)); 
+      } else if (input.GetPixel(row, col-1) == 0 && input.GetPixel(row-1, col) != 0) { // check top cell
+        input.SetPixel(row,col, input.GetPixel(row-1, col)); 
+      } else {
+        input.SetPixel(row,col, current_pixel_label);
+        current_pixel_label-=5; 
+        cout << current_pixel_label << "\n";
       }
     }
   }
 }
+
 
 // @brief Implementation of sequential labeling algorithm
 // @param input_filename the name of the input image
@@ -90,12 +83,15 @@ void PerformSequentialLabeling(const string &input_filename, const string &outpu
 
   for (int i = 0; i < input_rows; ++i) {
     for (int j = 0; j < input_cols; ++j) {
-      PixelLabeler(input, i, j, current_label);
-      cout << "Current Pixel: (" << i << ", " << j << ") | ";
-      cout << "Label: " << current_label << "\n";
+      if(input.GetPixel(i, j) != 0) {
+        PixelLabeler(input, i, j, current_label);
+        // cout << "Current Pixel: (" << i << ", " << j << ") | ";
+        // cout << "Label: " << current_label << "\n";
+      }
     }
   }
 
+  WriteImage(output_filename, input);
 }
 
 int main(int argc, char **argv){
